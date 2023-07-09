1a95c7a84ad56d7e23f060287eea93523e880c64
AmeyaGurjar <amey992004@gmail.com>
2023-07-10 02:42:33 +0530

    Meow

--- js/index.js
+++ js/index.js
@@ -1,2 +1,7 @@
 const FelineGit = "https://github.com/Project-FelineX";
 const FelineDevicesGit = "https://github.com/ProjectFelineDevices";
+
+const uiShift = () => {
+    var mainView = document.getElementById("mainView");
+    mainView.style="transform:translateY(0%);-ms-transform:translateY(0%)";
+}
