2a63bd6663139372eeae315e0aa88a3f41db6552
AmeyaGurjar <amey992004@gmail.com>
2023-07-10 03:44:46 +0530

    Meow

--- js/index.js
+++ js/index.js
@@ -3,5 +3,5 @@ const FelineDevicesGit = "https://github.com/ProjectFelineDevices";
 
 const uiShift = () => {
     var mainView = document.getElementById("mainView");
-    mainView.style="position:relative;transform:none;-ms-transform:none";
+    mainView.style="position:initial;transform:initial;";
 }
