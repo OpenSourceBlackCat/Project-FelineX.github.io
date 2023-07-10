cc183543150f94443d4c2ecde17e98d5e20f8c12
AmeyaGurjar <amey992004@gmail.com>
2023-07-10 15:42:28 +0530

    Meow

--- js/index.js
+++ js/index.js
@@ -5,8 +5,16 @@ const uiShift = (btn) => {
     var mainView = document.getElementById("mainView");
     //mainView.style="animation-name:mainViewAnim;animation-duration:5s;"
     mainView.style="position:initial;transform:initial;";
-    document.getElementById(btn).style="display:none";
+    document.getElementById(btn).style="display:none;";
 }
 const colorMode = (btn) => {
-    document.getElementById(btn).src = "assets/light.png";
+    var mainImg = document.getElementById(btn).src;
+    if (mainImg=="dark.png8"){
+        mainImg = "../assets/light.png";
+        document.body.style="background-color:black;";
+    }
+    else{
+        mainImg = "../assets/dark.png";
+        document.body.style="background-color:initial;";
+    }
 }
